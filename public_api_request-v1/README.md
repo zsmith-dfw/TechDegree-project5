cd185020d231ef680b4710014b638f1a5c5370f3
Zach Smith <rysskismith@gmail.com>
2020-07-13 17:47:31 -0500

    ready for pre-review

---
+++ public_api_request-v1/README.md
@@ -0,0 +1,4 @@
+This is a simple gallery application that can showcase employees of a company. As a placeholder, the name "Initech" is used. You may recognize this from the 1999 cult classic film "Office Space"
+
+Simply click on an employee and a window will populate with additional information. Close the window, and the user may select another employee. This data is pulled in JSON format from the random user generator API. As of now, this application requests generated data for 12 employees. To tailor the request, please view the documentation for this API at https://randomuser.me/documentation#results.
+
--- public_api_request-v1/js/script.js
+++ public_api_request-v1/js/script.js
@@ -1,68 +1,142 @@
-const gallery = document.getElementById('gallery')
-const body = document.querySelector('body')
-console.log(body)
+const gallery = document.getElementById("gallery");
+const body = document.querySelector("body");
 
 // ------------------------------------------
-//  FETCH FUNCTIONS
-// ------------------------------------------
-fetch('https://randomuser.me/api/?results=12')
-  .then(response => response.json())
-  .then(data => generateCards(data.results))
-
-// ------------------------------------------
-//  HELPER FUNCTION
+//  FETCH FUNCTION
 // ------------------------------------------
 
-function generateCards(data){
+fetch("https://randomuser.me/api/?results=12")
+  .then((response) => response.json())
+  .then((data) => generateCards(data.results));
 
-  data.map(employee => {
-  const card = document.createElement('div')
-  card.className = 'card'
-  gallery.appendChild(card)
+// ------------------------------------------
+//  GENERATES ALL EMPLOYEE CARDS IN GALLERY
+// ------------------------------------------
 
-card.innerHTML =`
+function generateCards(data) {
+  data.map((employee) => {
+    const card = document.createElement("div");
+    card.className = "card";
+    gallery.appendChild(card);
 
-`;
-const imageContainer = document.createElement('div')
-imageContainer.className = 'card-img-container'
-card.appendChild(imageContainer)
+    card.innerHTML = ``;
 
-const profilePic = document.createElement('img')
-profilePic.className = 'card-img'
-imageContainer.appendChild(profilePic)
-profilePic.src = `${employee.picture.large}`
+    const imageContainer = document.createElement("div");
+    imageContainer.className = "card-img-container";
 
-const cardInfo = document.createElement('div')
-card.appendChild(cardInfo)
-cardInfo.className = 'card-info-container'
-cardInfo.innerHTML = `<h3>${employee.name.first} ${employee.name.last}</h3>
-                      <p>${employee.email}</p>
-                      <p>${employee.location.city}, ${employee.location.state}</p>`
-                      console.log(card)
+    const profilePic = document.createElement("img");
+    profilePic.className = "card-img";
+    profilePic.src = `${employee.picture.large}`;
 
-  card.addEventListener('click', (event) =>{
-    const modalContainer = document.createElement('div')
-    body.appendChild(modalContainer)
-    modalContainer.className ='modal-container'
-    console.log(modalContainer)
+    const cardInfo = document.createElement("div");
+    cardInfo.className = "card-info-container";
 
-    const modal = document.createElement('div')
-    modalContainer.appendChild(modal)
-    modal.className ='modal'
+    const cardEmail = document.createElement("p");
+    cardEmail.className = "card-text";
+    cardEmail.innerHTML = `${employee.email}`;
 
-    const modalData = document.createElement('div')
-    modal.appendChild(modalData)
-    modalData.className = 'modal-info-container'
+    const cardLocation = document.createElement("p");
+    cardLocation.className = "card-text";
+    cardLocation.innerHTML = `${employee.location.city}, ${employee.location.state}`;
 
-    const modalPic = document.createElement('img')
-    modalPic.className = 'modal-img'
-    modalData.appendChild(modalPic)
-    console.log(modalPic)
-    modalPic.src =`${employee.picture.large}`
-    modalData.innerHTML = `<h3>${employee.name.first} ${employee.name.last}</h3>`
-    event.target.style.display = 'flex'
+    cardInfo.innerHTML = `<h3>${employee.name.first} ${employee.name.last}</h3>`;
 
-    })
-})
+    card.appendChild(imageContainer);
+    imageContainer.appendChild(profilePic);
+    card.appendChild(cardInfo);
+    cardInfo.appendChild(cardEmail);
+    cardInfo.appendChild(cardLocation);
+
+    // ------------------------------------------
+    //  MODAL LISTENER
+    // ------------------------------------------
+
+    card.addEventListener("click", () => {
+      // ------------------------------------------
+      //  MODAL - BEFORE HORIZONTAL RULE
+      // ------------------------------------------
+
+      const modalContainer = document.createElement("div");
+
+      modalContainer.className = "modal-container";
+      console.log(modalContainer);
+
+      const modal = document.createElement("div");
+      modal.className = "modal";
+
+      const closeButton = document.createElement("button");
+      closeButton.className = "modal-close-btn";
+      closeButton.innerHTML = "X";
+
+      const modalData = document.createElement("div");
+      modalData.className = "modal-info-container";
+      const modalName = document.createElement("h3");
+      modalName.className = "modal-name";
+      const modalPic = document.createElement("img");
+      modalPic.className = "modal-img";
+      modalPic.src = `${employee.picture.large}`;
+      const modalTextEmail = document.createElement("p");
+      const modalTextCity = document.createElement("p");
+      modalTextEmail.className = "modal-text";
+      modalTextCity.className = "modal-text";
+      modalName.innerHTML = `${employee.name.first} ${employee.name.last}`;
+      modalTextEmail.innerHTML = `${employee.email}`;
+      modalTextCity.innerHTML = `${employee.location.city}`;
+      const hr = document.createElement("hr");
+
+      body.appendChild(modalContainer);
+      modalContainer.appendChild(modal);
+      modal.appendChild(modalData);
+      modal.appendChild(closeButton);
+      modalData.appendChild(modalPic);
+      modalData.appendChild(modalName);
+      modalData.appendChild(modalTextEmail);
+      modalData.appendChild(modalTextCity);
+      modalData.appendChild(hr);
+
+      // ------------------------------------------
+      //  DATE FORMATTING
+      // ------------------------------------------
+
+      let date = new Date(employee.dob.date);
+      date = date.toISOString().slice(0, 10);
+
+      function formatDate(input) {
+        const newDate = input.match(/\d+/g),
+          year = newDate[0].substring(0, 4),
+          month = newDate[1],
+          day = newDate[2];
+        return day + "/" + month + "/" + year;
+      }
+
+      // ------------------------------------------
+      //  MODAL - AFTER HORIZONTAL RULE
+      // ------------------------------------------
+
+      const phone = document.createElement("p");
+      phone.className = "modal-text";
+      phone.innerHTML = `${employee.phone}`;
+      const address = document.createElement("p");
+      address.className = "modal-text";
+      address.innerHTML = `${employee.location.street.number} ${employee.location.street.name} ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}`;
+      const birthday = document.createElement("p");
+      birthday.className = "modal-text";
+      birthday.innerHTML = `Birthday: ${formatDate(date)}`;
+
+      modalData.appendChild(phone);
+      modalData.appendChild(address);
+      modalData.appendChild(birthday);
+
+      // ------------------------------------------
+      //  CLOSE LISTENER
+      // ------------------------------------------
+
+      closeButton.addEventListener("click", (event) => {
+        if (event.target) {
+          modal.style.display = "none";
+          modalContainer.className = "";
+        }
+      });
+    });
+  });
 }
-
